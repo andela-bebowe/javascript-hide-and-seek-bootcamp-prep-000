c6f5658d2c85603aeb363be818baaf8366dad06c
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-16 20:31:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  var current = document.querySelectorAll('div#grand-node')
+  var current = document.querySelectorAll('div#grand-node div')
   var next = []
 
   while (current) {
+    if(current.length == 1) {
+      return current[0]
+    }
+
     for (var i = 0; i < current.length; i++) {
       next.push(current[i])
     }
     current = next.shift()
     current = current.querySelectorAll('div')
-    if(current.length == 1) {
-      return current[0]
-    }
   }
 }
